b0fdf39e0f79f073ca843d66b5f0858562c515b1
LionKig <kevinmorillowupwork@gmail.com>
2021-07-07 03:26:46 -0700

    202107070326-2-3

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,16 @@
 const path = require('path');
-const webpack = require('webpack');
 
 const config = {
-  resolve: {
-    modules: [path.resolve('./lib'), path.resolve('./node_modules')]
-  },
-  entry: {
-    vendor: [
-      'babel-polyfill',
-      'react',
-      'react-dom',
-      'prop-types',
-      'axios',
-      'lodash.debounce',
-      'lodash.pickby'
-    ],
-    app: ['./lib/renderers/dom.js']
-  },
+  entry: ['babel-polyfill', './lib/components/Index.js'],
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: '[name].js'
+    filename: 'bundle.js'
   },
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-        options: {
-          presets: ['react', 'env', 'stage-2']
-        }
-      }
-    }]
-  },
-  plugins: [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor'
-    })
-  ]
+    rules: [
+      {test: /\.js$/, exclude: /node_modules/, use: 'babel-loader'}
+    ]
+  }
 };
 
 module.exports = config;
